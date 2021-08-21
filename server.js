9e51e34911a09c048577d1594ade8d0ee63be17d
Your Name <robertcjohnson1984@gmail.com>
2021-08-21 01:36:54 -0600

    update routes

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker
 );
 
 // routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/html.js"))
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"))
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
