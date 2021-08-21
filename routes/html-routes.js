5bf569ff56a994d269fa6be1fa5089167ac3e386
Your Name <robertcjohnson1984@gmail.com>
2021-08-20 23:09:46 -0600

    create html routes from public files

---
+++ routes/html-routes.js
@@ -0,0 +1,18 @@
+const express= require('express');
+const router = express.Router();
+const path = require('path');
+
+router.get('/exercise', (req, res)=>{
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
+})
+
+router.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+})
+
+router.get('/stats', (req, res)=>{
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
+})
+
+
+module.exports=router;
