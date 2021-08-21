c3642a1c9c107f23a07fd3af0da62732ea3bef9f
Your Name <robertcjohnson1984@gmail.com>
2021-08-20 23:01:31 -0600

    add req morgan

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
